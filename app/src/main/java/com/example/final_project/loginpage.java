b5d0fad3fac949c57c9811bb3a40e17ab55a4ad0
jadberjawi <jadberjawi9@gmail.com>
2021-12-09 00:50:34 +0200

    more style changes

--- app/src/main/java/com/example/final_project/loginpage.java
+++ app/src/main/java/com/example/final_project/loginpage.java
@@ -3,20 +3,84 @@ package com.example.final_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class loginpage extends AppCompatActivity {
     Button b;
+    EditText usern,pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         b = (Button) findViewById(R.id.button2);
+        usern=(EditText) findViewById(R.id.editTextTextPersonName7);
+        pass=(EditText) findViewById(R.id.editTextTextPassword);
     }
+
     public void starterpage(View view) {
         Intent i = new Intent(loginpage.this, starterpage.class);
         startActivity(i);
+        DownloadName task = new DownloadName();
+
+    }
+
+    public class DownloadName extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection;
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                int data = reader.read();
+                while (data != -1) {
+
+                    result = result + reader.readLine();
+                    data = reader.read();
+                }
+                //Toast.makeText(MainActivity.this,"reached 1", Toast.LENGTH_LONG).show();
+
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONArray jsonarray = new JSONArray(s);
+
+                for(int i=0; i < jsonarray.length(); i++) {
+                    JSONObject jsonobject = jsonarray.getJSONObject(i);
+                    String age    = jsonobject.getString("age");
+                    String firstname = jsonobject.getString("First_Name");
+                    t.setText(age+" "+firstname);
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/final_project/signuppage.java
+++ app/src/main/java/com/example/final_project/signuppage.java
@@ -8,12 +8,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class signuppage extends AppCompatActivity {
-Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signuppage);
-        b=(Button) findViewById(R.id.signup);
     }
     public void starterpage(View view) {
         Intent i = new Intent(signuppage.this, starterpage.class);
